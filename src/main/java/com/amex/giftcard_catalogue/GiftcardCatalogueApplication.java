e64a0a0e94380ef5aaeea2b2b272d6236c757197
Hibah Abid <hibah28@live.co.uk>
2024-08-15 19:20:24 +0100

    project setup springboot & mvn

---
+++ src/main/java/com/amex/giftcard_catalogue/GiftcardCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.amex.giftcard_catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftcardCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftcardCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amex/giftcard_catalogue/GiftcardCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amex.giftcard_catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftcardCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
